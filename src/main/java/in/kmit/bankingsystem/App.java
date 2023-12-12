874958f16a0981a1d8c567f738c2bb862bf414ae
udaykiran-29 <udayguttula19221@gmail.com>
2023-12-12 10:26:18 +0530

    Demo Project for Team

---
+++ src/main/java/in/kmit/bankingsystem/App.java
@@ -0,0 +1,14 @@
+package in.kmit.bankingsystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello" );
+    }
+}
+
